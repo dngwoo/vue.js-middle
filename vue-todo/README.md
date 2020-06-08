606cb532807d8a806bdb7e03cfbcce8398742bf8
dngwoo <akfmzk84@naver.com>
2020-06-08 10:53:00 +0900

    TodoList 컴포넌트의 할 일 목록 표시 기능 구현

--- vue-todo/README.md
+++ vue-todo/README.md
@@ -5,4 +5,6 @@
 - [x] 컴포넌트 생성 및 등록하기
 - [x] 파비콘, 아이콘, 폰트, 반응형 태그 설정하기
 - [x] TodoHeader.vue
-- [x] TodoInput.vue 저장기능 구현
+- [x] TodoInput.vue 저장기능 구현 (v-model, localStroage.setItem(key,value))
+- [x] TodoInput.vue 코드 분할 및 UI 스타일링
+- [x] TodoList.vue 할 일 목록 표시 (v-for, localStorage.key(i))
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,10 +1,35 @@
 <template>
-  <div>List</div>
+  <div>
+    <ul>
+      <li v-for="todoItem in todoItems" v-bind:key="todoItem">{{todoItem}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function() {
+    return {
+      todoItems: []
+    };
+  },
+  created: function() {
+    // 인스턴스가 생성되자 말자 호출되는 라이프싸이클 훅임.
+    // 훅이란 생성되는 시점에 이 안에 로직이 실행되는 것임.
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+          console.log(localStorage.key(i));
+          this.todoItems.push(localStorage.key(i));
+        }
+      }
+    }
+  }
+};
 </script>
 
 <style>
 </style>
+
+
+
