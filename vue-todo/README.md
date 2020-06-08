d0c439ad038dd6c357dd112efdf68f26d470a21a
dngwoo <akfmzk84@naver.com>
2020-06-08 09:20:30 +0900

    TodoInput.vue 할 일 저장 기능 구현

--- vue-todo/README.md
+++ vue-todo/README.md
@@ -5,3 +5,4 @@
 - [x] 컴포넌트 생성 및 등록하기
 - [x] 파비콘, 아이콘, 폰트, 반응형 태그 설정하기
 - [x] TodoHeader.vue
+- [x] TodoInput.vue 저장기능 구현
--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>Input</div>
+  <div>
+    <!-- v-model을 사용하여 input과 data값을 동적으로 매핑 -->
+    <input type="text" v-model="newTodoItem" />
+    <button v-on:click="addTodo">add</button>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function() {
+    return {
+      newTodoItem: ""
+    };
+  },
+  methods: {
+    addTodo: function() {
+      // 저장하는 로직
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.newTodoItem = "";
+    }
+  }
+};
 </script>
 
 <style>
